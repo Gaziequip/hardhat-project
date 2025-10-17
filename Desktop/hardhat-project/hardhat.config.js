ed51e86a8ab78a4fbb5b8818e358503b6dc2d498
Gaziequip <Gazi_Equipments@proton.me>
2025-10-17 09:13:51 +0100

    Update hardhat.config.js

--- Desktop/hardhat-project/hardhat.config.js
+++ Desktop/hardhat-project/hardhat.config.js
@@ -1,13 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config(); // Load environment variables
+require("@nomicfoundation/hardhat-ethers");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    bsctest: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      accounts: [process.env.d7f29e8df968d002cb827ddd813477515f98852cd8ba78261981eb898ed23747], // Now correctly loaded
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
     },
-  },
+    bscMainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: ["f149ac9696b2095d05f39df411510e5ca0ab070496d9d7a319a25e1b9f36b650"] // Replace with your actual private key
+    }
+  }
 };
